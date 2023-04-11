ac6611c327d0ad49e13388a5498163b325bd9fee
Rishi Srivastava <rishisrivasta@gmail.com>
2023-04-12 07:50:50 +0800

    added false sharing code

---
+++ src/main/java/com/backstreetbrogrammer/ch02_orderingReadAndWrite/FalseSharingDemo.java
@@ -0,0 +1,16 @@
+package com.backstreetbrogrammer.ch02_orderingReadAndWrite;
+
+public class FalseSharingDemo {
+
+    public final static class VolatileLongPadded {
+        public long q1, q2, q3, q4, q5, q6;
+        public volatile long value = 0L;
+        public long q11, q12, q13, q14, q15, q16;
+
+    }
+
+    public final static class VolatileLongUnPadded {
+        public volatile long value = 0L;
+    }
+
+}
---
+++ src/test/java/com/backstreetbrogrammer/ch02_orderingReadAndWrite/FalseSharingDemoTest.java
@@ -0,0 +1,98 @@
+package com.backstreetbrogrammer.ch02_orderingReadAndWrite;
+
+import com.backstreetbrogrammer.ch02_orderingReadAndWrite.FalseSharingDemo.VolatileLongPadded;
+import com.backstreetbrogrammer.ch02_orderingReadAndWrite.FalseSharingDemo.VolatileLongUnPadded;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+
+public class FalseSharingDemoTest {
+
+    private VolatileLongPadded[] paddedLongs;
+    private VolatileLongUnPadded[] unPaddedLongs;
+
+    private final int numOfThreads = 4;
+    private final long iterations = 100_000_000L;
+
+    @BeforeEach
+    void setUp() {
+        paddedLongs = new VolatileLongPadded[]{
+                new VolatileLongPadded(),
+                new VolatileLongPadded(),
+                new VolatileLongPadded(),
+                new VolatileLongPadded()
+        };
+
+        unPaddedLongs = new VolatileLongUnPadded[]{
+                new VolatileLongUnPadded(),
+                new VolatileLongUnPadded(),
+                new VolatileLongUnPadded(),
+                new VolatileLongUnPadded()
+        };
+    }
+
+    @Test
+    @DisplayName("Demonstrate False Sharing")
+    void demonstrateFalseSharing() throws InterruptedException {
+        for (int i = 1; i <= numOfThreads; i++) {
+            final Thread[] threads = new Thread[i];
+            for (int j = 0; j < threads.length; j++) {
+                threads[j] = new Thread(createUnpaddedRunnable(j));
+            }
+            final Instant start = Instant.now();
+            for (final Thread t : threads) {
+                t.start();
+            }
+            for (final Thread t : threads) {
+                t.join();
+            }
+            final long timeElapsed = (Duration.between(start, Instant.now()).toMillis());
+            System.out.printf("[UNPADDED] No of Threads=[%d], total time taken: %d ms%n%n", i, timeElapsed);
+        }
+        System.out.println("------------------------------");
+    }
+
+    @Test
+    @DisplayName("Demonstrate fixing False Sharing by padding")
+    void demonstrateFixingFalseSharingByPadding() throws InterruptedException {
+        for (int i = 1; i <= numOfThreads; i++) {
+            final Thread[] threads = new Thread[i];
+            for (int j = 0; j < threads.length; j++) {
+                threads[j] = new Thread(createPaddedRunnable(j));
+            }
+            final Instant start = Instant.now();
+            for (final Thread t : threads) {
+                t.start();
+            }
+            for (final Thread t : threads) {
+                t.join();
+            }
+            final long timeElapsed = (Duration.between(start, Instant.now()).toMillis());
+            System.out.printf("[PADDED] No of Threads=[%d], total time taken: %d ms%n%n", i, timeElapsed);
+        }
+        System.out.println("------------------------------");
+    }
+
+    private Runnable createUnpaddedRunnable(final int k) {
+        return () -> {
+            long i = iterations + 1;
+            while (0 != --i) {
+                unPaddedLongs[k].value = i;
+            }
+        };
+    }
+
+    private Runnable createPaddedRunnable(final int k) {
+        return () -> {
+            long i = iterations + 1;
+            while (0 != --i) {
+                paddedLongs[k].value = i;
+            }
+        };
+    }
+
+
+}
